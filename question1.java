aedf91a03017bd1ea918c0aebb1419d13f44a967
gitdeepaks <100864803+gitdeepaks@users.noreply.github.com>
2022-08-19 18:18:52 +0530

    Assignment3
    Assignment Work

---
+++ question1.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class question1 {
+    public static void main(String[] args) {
+
+        //Conventional Loop Method
+
+        String s1 ="INeuron";
+        String s2 ="";
+
+
+        for (int i=s1.length()-1; i>=0;i--){
+            s2 = s2+s1.charAt(i);
+
+        }
+        System.out.println(s2);
+
+        //Using Reverse Methode
+        
+        StringBuilder sv = new StringBuilder("INeuron");
+        System.out.println(sv.reverse());
+
+
+    }
+}
