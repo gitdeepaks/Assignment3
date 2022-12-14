1ceed447acab28a89317b9851d176c80415dce18
gitdeepaks <deepaksankhyan92@outlook.com>
2022-12-14 12:09:08 +0530

    Adding pythone

---
+++ Anpa_Collage/FirstProgram.py
@@ -0,0 +1,20 @@
+# first = input("FIrstNo: ")
+# second = input("Second no: ")
+#
+# sum = int (first) + int(second)
+#
+# print (float(sum))
+# age = 2
+#
+# if age >= 18:
+#     print("you are eligible to cast vote")
+# elif age < 18 and  age > 3 :
+#     print("you are in sschool")
+#
+# else:
+#     print ("YOU ARE A KID, Please go to school FIRST and get good score ")
+#
+# for i in range (5):
+#     print(i +1)
+import  math
+print (dir (math) )
